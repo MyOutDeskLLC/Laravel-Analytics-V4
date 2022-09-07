cb18af859591133a8c8526520b35f3b8c9c48b0a
JL <jay.lindsley@gmail.com>
2022-09-07 15:17:35 -0700

    feat: support for limits, offsets, filtering, returning just data (no google mess)

--- src/LaravelAnalyticsV4.php
+++ src/LaravelAnalyticsV4.php
@@ -2,17 +2,67 @@
 
 namespace Myoutdeskllc\LaravelAnalyticsV4;
 
+use Google\Analytics\Data\V1beta\DimensionHeader;
+use Google\Analytics\Data\V1beta\DimensionValue;
+use Google\Analytics\Data\V1beta\MetricHeader;
+use Google\Analytics\Data\V1beta\MetricValue;
+use Google\Analytics\Data\V1beta\Row;
+use Google\Analytics\Data\V1beta\RunReportResponse;
+use Illuminate\Support\Collection;
+
 class LaravelAnalyticsV4
 {
     protected LaravelAnalyticsV4Client $client;
+    protected bool $shouldConvertResponseToArray = true;
 
     public function __construct(LaravelAnalyticsV4Client $client)
     {
         $this->client = $client;
     }
 
-    public function runReport(RunReportConfiguration $configuration)
+    public function convertResponseToArray(bool $convert = true) : static
     {
-        $this->client->runReport($configuration->toArray());
+        $this->shouldConvertResponseToArray = $convert;
+
+        return $this;
+    }
+
+    /**
+     * I dont like the GA return types so I'm going to convert it all to static data
+     *
+     * @param RunReportResponse $response
+     * @return array
+     */
+    public function unwrapToArray(RunReportResponse $response) : array
+    {
+        $dimensionHeaders = collect($response->getDimensionHeaders())->map(function(DimensionHeader $header) {
+            return $header->getName();
+        })->toArray();
+
+        $metricHeaders = collect($response->getMetricHeaders())->map(function(MetricHeader $header) {
+            return $header->getName();
+        })->toArray();
+
+        return collect($response->getRows())->map(function(Row $row) use ($dimensionHeaders, $metricHeaders) {
+            $finalData = [];
+
+            collect($row->getDimensionValues())->map(function(DimensionValue $rowValue, $rowIndex) use (&$finalData, $dimensionHeaders) {
+                $finalData['dimensions'][$dimensionHeaders[$rowIndex]] = $rowValue->getValue();
+            });
+
+            collect($row->getMetricValues())->each(function(MetricValue $metricValue, $rowIndex) use (&$finalData, $metricHeaders) {
+                $finalData['metrics'][$metricHeaders[$rowIndex]] = $metricValue->getValue();
+            });
+
+            return $finalData;
+        })->toArray();
+    }
+
+    public function runReport(RunReportConfiguration $configuration) : RunReportResponse | array
+    {
+        if(!$this->shouldConvertResponseToArray) {
+            return $this->client->runReport($configuration->toGoogleObject());
+        }
+        return $this->unwrapToArray($this->client->runReport($configuration->toGoogleObject()));
     }
 }
--- src/RunReportConfiguration.php
+++ src/RunReportConfiguration.php
@@ -2,14 +2,12 @@
 
 namespace Myoutdeskllc\LaravelAnalyticsV4;
 
-use Carbon\Carbon;
 use Google\Analytics\Data\V1beta\DateRange;
 use Google\Analytics\Data\V1beta\Dimension;
-use Google\Analytics\Data\V1beta\Filter;
 use Google\Analytics\Data\V1beta\FilterExpression;
 use Google\Analytics\Data\V1beta\FilterExpressionList;
 use Google\Analytics\Data\V1beta\Metric;
-use Illuminate\Support\Arr;
+use Google\Analytics\Data\V1beta\OrderBy;
 use Illuminate\Support\Str;
 use Myoutdeskllc\LaravelAnalyticsV4\Exceptions\InvalidDimensionException;
 use Myoutdeskllc\LaravelAnalyticsV4\Filters\DimensionFilter;
@@ -21,9 +19,14 @@ class RunReportConfiguration
     protected string $endDate;
     protected array $dimensions = [];
     protected array $metrics = [];
+    protected array $orderByMetrics = [];
+    protected array $orderByDimensions = [];
+    protected int $limit = -1;
+    protected int $offset = -1;
+
     /** @var DimensionFilter[] */
     protected array $filters = [];
-    protected string $filterMethod = 'andGroup';
+    protected string $filterMethod = 'and_group';
 
 
     public function setStartDate(string $startDate): static
@@ -66,7 +69,7 @@ class RunReportConfiguration
         return $this;
     }
 
-    public function addSingleFilter(DimensionFilter $filter)
+    public function addFilter(DimensionFilter $filter)
     {
         $this->filters[] = $filter;
     }
@@ -74,25 +77,52 @@ class RunReportConfiguration
     public function addAndFilterGroup(array $dimensionFilters)
     {
         $this->filters = $dimensionFilters;
-        $this->filterMethod = 'andGroup';
+        $this->filterMethod = 'and_group';
     }
 
     public function addOrFilterGroup(array $dimensionFilters)
     {
         $this->filters = $dimensionFilters;
-        $this->filterMethod = 'orGroup';
+        $this->filterMethod = 'or_group';
+    }
+
+    public function orderByMetric(string $metricName, bool $desc = false)
+    {
+        $this->orderByMetrics[$metricName] = [
+            'name' => $metricName,
+            'desc' => $desc
+        ];
+    }
+
+    public function orderByDimension(string $dimensionName, bool $desc = false)
+    {
+        $this->orderByDimensions[$dimensionName] = [
+            'name' => $dimensionName,
+            'desc' => $desc
+        ];
+    }
+
+    public function setLimit(int $limit)
+    {
+        $this->limit = $limit;
     }
 
     public function buildDimensionFilters()
     {
+        if(empty($this->filters)) {
+            return null;
+        }
         if(count($this->filters) === 1) {
             return new FilterExpression(['filter' => $this->filters[0]->getGoogleFilterType()]);
         }
-        if($this->filterMethod === 'andGroup') {
-            return new FilterExpressionList(['expressions' => collect($this->filters)->map(function(DimensionFilter $filter) {
-                return new FilterExpression(['filter' => $filter->getGoogleFilterType()]);
-            })->toArray()]);
-        }
+
+        return new FilterExpression([
+            $this->filterMethod => new FilterExpressionList([
+                'expressions' => collect($this->filters)->map(function(DimensionFilter $filter) {
+                    return new FilterExpression(['filter' => $filter->getGoogleFilterType()]);
+                })->toArray()
+            ])
+        ]);
     }
 
     public function buildNativeDateRange()
@@ -119,13 +149,44 @@ class RunReportConfiguration
         })->toArray();
     }
 
-    public function toArray()
+    public function buildNativeOrderBy()
     {
-        return [
+        $metricOrders = collect($this->orderByMetrics)->map(function($order) {
+                return (new OrderBy())->setMetric(new OrderBy\MetricOrderBy([
+                    'metric_name' => $order['name']
+                ]))->setDesc($order['desc']);
+        })->toArray();
+
+        $dimensionOrders = collect($this->orderByDimensions)->map(function($order) {
+            return (new OrderBy())->setDimension(new OrderBy\DimensionOrderBy([
+                'dimension_name' => $order['name']
+            ]))->setDesc($order['desc']);
+        })->toArray();
+
+        return [...$metricOrders, ...$dimensionOrders];
+    }
+
+    public function toGoogleObject()
+    {
+        $configuration = [
             'dateRanges' => $this->buildNativeDateRange(),
             'dimensions' => $this->buildNativeDimensions(),
             'metrics' => $this->buildNativeMetrics(),
             'dimensionFilter' => $this->buildDimensionFilters()
         ];
+
+        if(!empty($this->orderByMetrics)) {
+            $configuration['orderBys'] = $this->buildNativeOrderBy();
+        }
+
+        if($this->limit !== -1) {
+            $configuration['limit'] = $this->limit;
+        }
+
+        if($this->offset !== -1) {
+            $configuration['offset'] = $this->offset;
+        }
+
+        return $configuration;
     }
 }
