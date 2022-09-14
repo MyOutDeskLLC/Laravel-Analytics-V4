17bdc8fce72bdaaba87f0c218e9af88f1c0112df
mohammad javad seydi <seydi.birjand@gmail.com>
2022-09-14 11:20:33 +0430

    format config file

--- config/analytics-v4.php
+++ config/analytics-v4.php
@@ -1,13 +1,13 @@
 <?php
 
 return [
-    'property_id' => env('ANALYTICS_PROPERTY_ID', 'UA-XXXXXXXXX-X'),
+    'property_id' => env('ANA_PROPERTY_ID', 'XXXXXXXXX'),
     'service_account_credentials_json' => storage_path('app/analytics/service-account-credentials.json'),
     'cache' => [
-        'enableCaching' => env('ANALYTICS_CACHE',''),
-        'authCache' => env('ANALYTICS_AUTH_CACHE',''),
+        'enableCaching' => env('ANA_CACHE',false),
+        'authCache' =>null,
         'authCacheOptions' => [
-            'lifetime' => env('ANALYTICS_CACHE_LIFETIME', 60),
+            'lifetime' => env('ANA_CACHE_LIFETIME', 60),
             'prefix' => env('ANA_CACHE_PREFIX', 'analytics_'),
         ],
     ],
