989ee782697af61e76591635e1ac78a416f7a0c7
mohammad javad seydi <seydi.birjand@gmail.com>
2022-09-14 11:23:58 +0430

    update env keys

--- config/analytics-v4.php
+++ config/analytics-v4.php
@@ -1,14 +1,14 @@
 <?php
 
 return [
-    'property_id' => env('ANA_PROPERTY_ID', 'XXXXXXXXX'),
+    'property_id' => env('ANALYTICS_PROPERTY_ID', 'UA-XXXXXXXXX-X'),
     'service_account_credentials_json' => storage_path('app/analytics/service-account-credentials.json'),
     'cache' => [
-        'enableCaching' => env('ANA_CACHE',false),
-        'authCache' =>null,
+        'enableCaching' => env('ANALYTICS_CACHE',''),
+        'authCache' => env('ANALYTICS_AUTH_CACHE',''),
         'authCacheOptions' => [
-            'lifetime' => env('ANA_CACHE_LIFETIME', 60),
-            'prefix' => env('ANA_CACHE_PREFIX', 'analytics_'),
+            'lifetime' => env('ANALYTICS_CACHE_LIFETIME', 60),
+            'prefix' => env('ANALYTICS_CACHE_PREFIX', 'analytics_'),
         ],
     ],
 ];
