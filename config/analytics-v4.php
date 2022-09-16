75588b5e22318ce6c2264b300741757ee9c604d5
WalrusSoup <WalrusSoup@users.noreply.github.com>
2022-09-16 15:17:46 +0000

    Fix styling

--- config/analytics-v4.php
+++ config/analytics-v4.php
@@ -4,7 +4,7 @@ return [
     'property_id' => env('ANALYTICS_PROPERTY_ID', 'XXXXXXXXX'),
     'service_account_credentials_json' => storage_path('app/analytics/service-account-credentials.json'),
     'cache' => [
-        'enableCaching' => env('ANALYTICS_CACHE',false),
+        'enableCaching' => env('ANALYTICS_CACHE', false),
         'authCache' => null,
         'authCacheOptions' => [
             'lifetime' => env('ANALYTICS_CACHE_LIFETIME', 60),
