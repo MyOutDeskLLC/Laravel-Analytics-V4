ebeabc39f53f9b1a09a686af417ae98275d1c04a
JL <jay.lindsley@gmail.com>
2022-09-21 08:53:07 -0700

    fix: unsafe usage of new static

--- src/Exceptions/InvalidPeriodException.php
+++ src/Exceptions/InvalidPeriodException.php
@@ -9,6 +9,6 @@ class InvalidPeriodException extends Exception
 {
     public static function startDateCannotBeAfterEndDate(DateTimeInterface $startDate, DateTimeInterface $endDate): self
     {
-        return new static("Start date `{$startDate->format('Y-m-d')}` cannot be after end date `{$endDate->format('Y-m-d')}`.");
+        return new self("Start date `{$startDate->format('Y-m-d')}` cannot be after end date `{$endDate->format('Y-m-d')}`.");
     }
 }
